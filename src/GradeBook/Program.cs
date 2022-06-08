8448aa0556f4879ac87ac3357f6b113519633da6
Kim Steen Bekke <65952389+KimSteenBekke@users.noreply.github.com>
2022-06-08 21:40:18 +0200

    Localmac (#2)
    * init
    
    * string args

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, Morten!");
+
+// Using System;
+
+// Namespace GradeBook
+// {
+
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"Hello, {args[0]} !");
+		}
+	}
+// }
